3ff2d960bf92e076e766907b5239922ed4e66e6c
prashant <prashantsharma98711@gmail.com>
2022-12-11 17:45:44 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/header';
+import logo from "./logo.svg";
+import React , { Component}  from 'react'
+import "./App.css";
+import Header from "./components/header";
 //import F from './pages/firstPage';
-import FirstPage from './pages/firstPage';
-import { StrictMode } from 'react';
+import FirstPage from "./pages/firstPage";
+//import { StrictMode } from 'react';
 
 function App() {
   return (
     <div>
-      <StrictMode>
-      <FirstPage/>
-      </StrictMode>
-      </div>
- 
-    //     <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/header" element={<Header />}>
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-  
+      <FirstPage />
+    </div>
   );
 }
 
