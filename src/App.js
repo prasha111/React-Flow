629626921e2657b9136eed11a3c6cfe6cc62b951
prash_1 <prashantsharma_2k17ee157@dtu.ac.in>
2022-12-14 20:27:51 +0530

    minor fixs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./components/header";
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
 //import F from './pages/firstPage';
-import FirstPage from "./pages/firstPage";
-import { StrictMode } from "react";
+import FirstPage from './pages/firstPage';
+import { StrictMode } from 'react';
+import { MessageProvider } from './components/messageProvider';
 
 function App() {
   return (
-    <>
-      <FirstPage />
-    </>
+    <div>
+      <StrictMode>
+        <MessageProvider>
+        <FirstPage/>
+        </MessageProvider>
+      </StrictMode>
+      </div>
+ 
+    //     <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/header" element={<Header />}>
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+  
   );
 }
 
